edc339a4dab2454a93806e94e29c31039590c798
Shubham Yadav <shubhamyadav3521@gmail.com>
2025-06-30 12:26:57 +0530

    prediction is done project completed

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import pickle
-from flask import Flask, request, app, redirect, url_for, render_template, jsonify
+from flask import  Flask,request,app,jsonify,url_for,render_template
+
 import numpy as np
 import pandas as pd
 
-
-app = Flask(__name__)
-model = pickle.load(open('logisticreg.pkl', 'rb'))
-scalar = pickle.load(open('scaler.pkl', 'rb'))
+app=Flask(__name__)
+#Load the Model
+regmodel = pickle.load(open("logisticreg.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
+encoders = pickle.load(open('label_encoders.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -14,27 +16,47 @@ def home():
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
-    """here we get the predict data so we have to scale the data first and convert it into 2D array"""
     data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    predict_data = np.array(list(data.values())).reshape(1,-1)
-    new_data = scalar.transform(predict_data)
-    output = model.pridict(new_data)
-    print(output[0])
-
+    print("data========",data)
+    print("erray=========",np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
+    print("output============",output[0])
     return jsonify(output[0])
 
-
-@app.route("/predict", methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data = [float(x) for x in request.form.values()]
-    final_input = scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output = model.pridict(final_input)[0]
+    data = request.form.to_dict()
+    print("data****************",data)
+    categorical_features = ['BusinessTravel', 'Department', 'EducationField', 'Gender',
+                        'JobRole', 'MaritalStatus', 'Over18', 'OverTime']
 
-    return render_template("home.html", prediction_text="the predict value is {}".format(output))
+    numerical_features = ['Age', 'DailyRate', 'DistanceFromHome', 'Education', 'EmployeeCount',
+                        'EmployeeNumber', 'EnvironmentSatisfaction', 'HourlyRate',
+                        'JobInvolvement', 'JobLevel', 'JobSatisfaction', 'MonthlyIncome',
+                        'MonthlyRate', 'NumCompaniesWorked', 'PercentSalaryHike',
+                        'PerformanceRating', 'RelationshipSatisfaction', 'StandardHours',
+                        'StockOptionLevel', 'TotalWorkingYears', 'TrainingTimesLastYear',
+                        'WorkLifeBalance', 'YearsAtCompany', 'YearsInCurrentRole',
+                        'YearsSinceLastPromotion', 'YearsWithCurrManager']
+    
+    encoded_cats = []
+    for col in categorical_features:
+        encoder = encoders[col]
+        val = data[col]
+        encoded_val = encoder.transform([val])[0]
+        encoded_cats.append(encoded_val)
 
-if __name__ == "__main__":
+    numerical_vals = [float(data[col]) for col in numerical_features]
+    final_input = np.array(encoded_cats + numerical_vals).reshape(1, -1)
+    
 
+    scaled_input  = scaler.transform(final_input)
+    print("final input =====", scaled_input )
+    output = regmodel.predict(scaled_input )[0]
+    predicted_label = "Yes" if output == 1 else "No"
+
+    return render_template("home.html", prediction_text="The prediction is employee leave the company- {}".format(predicted_label))
+
+if __name__=="__main__":
     app.run(debug=True)
